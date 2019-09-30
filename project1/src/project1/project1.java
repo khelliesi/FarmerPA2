feae92c48636f5b35a3314bd7b291152673ba71d
Kellie Farmer <kellie.farmer@knights.ucf.edu>
2019-09-30 19:04:53 -0400

    initial commit

---
+++ project1/src/project1/project1.java
@@ -0,0 +1,29 @@
+package project1;
+import java.util.Scanner;
+public class project1 {
+
+	
+	
+	public static void main(String[] args) {
+	//Encryption
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter your name:");
+		String name = in.next();
+		System.out.println("Hello, " + name);
+
+		System.out.print("Please enter your four digit integer:");
+		int number = in.nextInt();
+		System.out.println("Thank you, " + name + ".\nYou entered: " + number);
+		
+		
+		
+		
+		System.out.println("Encryption: ");
+		
+//Decryption
+		
+		System.out.println("Decryption: ");
+		
+		
+	}
+}
